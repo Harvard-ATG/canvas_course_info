44a6018dcaa23b017cb312585847811b9dc68cac
Christian Ondaatje <christian.ondaatje@gmail.com>
2015-06-22 14:42:45 -0400

    Trying to get some text formatting back in here.

--- course_info/views.py
+++ course_info/views.py
@@ -91,7 +91,7 @@ def __mungeFields(fields):
         field['value'] = field['value'].replace('<br /> <br />', '<br />')
     #fields['style'] = ["<style>testing testing hey what's up hello</style>"]
     print(fields)
-    fields.append({'value': u'<style>#heywhatsuphellostyle{color:fetty}</style>', 'key': 'style', 'class': 'course_info_textHeader1'})
+    fields.append({'value': '<style>#heywhatsuphellostyle{color:fetty}</style>', 'key': 'style', 'class': 'course_info_textHeader1'})
     return fields
 
 
