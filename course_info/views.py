75064f06e797e6d97c9bbea3f3cc5d83f8235ff8
Christian Ondaatje <christian.ondaatje@gmail.com>
2015-06-25 14:31:38 -0400

    Cleanup and Commenting

--- course_info/views.py
+++ course_info/views.py
@@ -85,6 +85,7 @@ def __course_context(request, course_instance_id, keys):
 
 def __mungeFields(fields, school_name):
     # Could possibly sneak in some more inline styles here
+
     def stern(value, default):
         # special ternary to simplify the logic below -deals with blank fields
         if value == u' ' or value == u'' or value == '' or value == ' ':
@@ -128,52 +129,81 @@ def __mungeFields(fields, school_name):
 def widget(request):
     course_instance_id = request.GET.get('course_instance_id')
     context = __course_context(request, course_instance_id, request.GET.getlist('f'))
-
-    # Why doesn't this work? I want to take __mungeFields out from the context creator so
-    # it doesn't mess up the editor but it somehow doesn't work like this
-    #context['fields'] = __mungeFields(context['fields'])
     return render(request, 'course_info/widget.html', context)
 
 def editor(request):
+
+    #TODO: could/should this be adapted to react to the environment? (Like never do this in production)
     if settings.COURSE_INSTANCE_ID:
-        # course_instance_id = "312976"
         course_instance_id = settings.COURSE_INSTANCE_ID
     else:
         course_instance_id = request.POST.get('lis_course_offering_sourcedid')
-    keys = ['title', 'course.registrar_code_display', 'term.display_name', 'instructors_display', 'location',
-            'meeting_time', 'exam_group', 'description', 'notes']
+
+    # The values we will want to display
+    keys = [
+        'title',
+        'course.registrar_code_display',
+        'term.display_name',
+        'instructors_display',
+        'location',
+        'meeting_time',
+        'exam_group',
+        'description',
+        'notes'
+    ]
+
     course_context = __course_context(request, course_instance_id, keys)
+
+        # this is likely a remnant of the iFrame resize struggle
     # course_context['line_guestimate'] =keys*2
+
     course_context['launch_presentation_return_url'] = request.POST.get('launch_presentation_return_url')
     course_context['should_offer_text'] = settings.OFFER_TEXT
 
+    # scrub HTML tags (just for the editor view)
     for field in course_context['fields']:
-        #abstracting out the 'clean' implementation
         field['value'] = striptags(field['value'])
 
     return render(request, 'course_info/editor.html', course_context)
 
 
 def oembed_handler(request):  # TODO
-    # This view handles the huge url Canvas throws at us, reconciles it with iCommons,
-    # and returns oEmbed JSON for the Canvas Rich Text Editor
-    # www.oembed.com
+    '''
+        This view handles the huge url Canvas throws at us, reconciles it with iCommons,
+        and returns oEmbed JSON for the Canvas Rich Text Editor
+        See the README for more information - how oEmbed works, terminology, etc.
+        www.oembed.com
+    '''
 
+
+    # the url we need to act on is a parameter in Canvas' oEmbed request
     url = request.GET.get('url')
+
+    # move from string functionality to url functionality
     parsed_url = urlparse.urlparse(url)
+
+    # load the url parameters/queries into a dictionary
     parsed_qs = urlparse.parse_qs(parsed_url.query)
-    requested_info = parsed_qs['f'] #get all the selected checkboxes from the editor view
+
+    # get the selected checkboxes and course id
+    requested_info = parsed_qs['f']
     course_instance_id = parsed_qs['course_instance_id'][0]
+
     course_info_context = __course_context(request, course_instance_id, requested_info)
 
-    # unfortunately the content-type is going to display because of the way the render function works
-    # hacky workaround is to remove the first 13 characters from the html_string. Sorry
-    html_string = str(render(request, 'course_info/widget.html', course_info_context, content_type= " "))
-    html_string = html_string[13:]
+    # put the rendered html in a string, so that it can be returned in the oEmbed JSON
+    # the content_type = " " is included so there will be a fixed amount of characters to delete later (13)
+    html_string = str(render(request, 'course_info/widget.html', course_info_context, content_type = " "))
 
-    #Returning just enough oEmbed to satisfy Canvas
-    response = json.JSONEncoder().encode({
+         # TODO: see if this can be improved
+    # unfortunately the content-type is going to display because of the way the render function works
+    # magic number 13 is the amount of characters in
+    # hacky workaround is to remove the first 13 characters from the html_string. Sorry
+    # html_string = html_string[13:]
+
+    # Returning just enough oEmbed to satisfy Canvas
+    oEmbed_response = json.JSONEncoder().encode({
         "html": html_string,
         "type": "rich"
     })
-    return HttpResponse(response, content_type="application/json")
+    return HttpResponse(oEmbed_response, content_type="application/json")
