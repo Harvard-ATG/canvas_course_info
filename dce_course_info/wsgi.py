b993a347d79484190b6216006105d1783b50f4e3
Christian Ondaatje <christian.ondaatje@gmail.com>
2015-06-26 16:38:23 -0400

    cleaned the dirty secret key nixing

--- dce_course_info/wsgi.py
+++ dce_course_info/wsgi.py
@@ -18,7 +18,4 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dce_course_info.settings.aws")
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
-#application = get_wsgi_application()
+application = get_wsgi_application()
