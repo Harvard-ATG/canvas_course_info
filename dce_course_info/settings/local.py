d789374bfe15fb70f789eff1aeb33abde4789bb4
Christian Ondaatje <christian.ondaatje@gmail.com>
2015-06-29 10:27:26 -0400

    Making more changes to align with TLT design spec

---
+++ dce_course_info/settings/local.py
@@ -0,0 +1,10 @@
+from .aws import *
+
+ENV_NAME = 'local'
+INSTALLED_APPS += ('debug_toolbar')
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+# For Django Debug Toolbar:
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
--- dce_course_info/wsgi.py
+++ dce_course_info/wsgi.py
@@ -18,7 +18,5 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dce_course_info.settings.aws")
-#application = get_wsgi_application()
 from dj_static import Cling
-#
 application = Cling(get_wsgi_application())
