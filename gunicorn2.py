66772fe15654d103d7bf9dc7712e26046ebe32f9
Christian Ondaatje <christian.ondaatje@gmail.com>
2015-07-01 14:33:23 -0400

    Ready for review.

--- gunicorn2.py
+++
@@ -1,4 +0,0 @@
-#from dce_course_info.settings import aws as settings
-
-workers = 4 #settings.GUNICORN_WORKERS #env('GUNICORN_WORKERS', 4)
-timeout = 60 #settings.GUNICORN_TIMEOUT #env('GUNICORN_TIMEOUT', 60)
