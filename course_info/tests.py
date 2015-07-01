a38d2a626f89cf2a31403589ff36fffed9e8ba71
Christian Ondaatje <christian.ondaatje@gmail.com>
2015-07-01 13:56:46 -0400

    Ready for review

--- course_info/tests.py
+++ course_info/tests.py
@@ -1,12 +1,20 @@
-# ./manage.py test
-
+# To test locally, run ./manage.py test
+# TODO: The test cases for this app are a little different than a typical app
+# because it's so network-heavy. It may be a good idea to do tests on each individual view
+# by sending in an appropriate request, then checking the response.
 
 from django.test import TestCase
 from icommons import ICommonsApi
 from django.test import Client
 
 # TODO: Expand test coverage
+
+
 class testData(TestCase):
+    '''
+        These tests will ensure that the widget (and by extension, the iCommons API) is running
+        as expected
+    '''
     # Tip: first word in a test method must be "test" for Django to recognize it & run it.
     def test_course_number(self):
         # Make sure that the widget is displaying a correct course number for a given course ID
@@ -21,6 +29,16 @@ class testData(TestCase):
         c = Client()
         response = c.get('/course_info/widget.html?course_instance_id=' + id + '&f=course.registrar_code_display')
         self.assertContains(response, "Harvard Divinity School")
+
+
+class testEditor(TestCase):
+    '''
+        TODO
+    '''
+
+    def test_insert_text_button_existence(self):
+        # make sure that the editor is responding to the "offer_text" setting
+        pass
 
 SCHOOLS = {
     "hsph":     "Harvard School of Public Health",
