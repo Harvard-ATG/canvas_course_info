b0199ba97c757614eda24543257d8c6854c1cf91
condaatje <condaatje@users.noreply.github.com>
2015-07-02 10:09:57 -0400

    Comment out non-working settings import

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,7 @@
 # from getenv import env
 # import dotenv
 # dotenv.read_dotenv()
-from dce_course_info.settings import aws as settings
+# from dce_course_info.settings import aws as settings
 
 #TODO: can we just delete this file? - TLT isn't going to use Gunicorn.
 # settings.GUNICORN_WORKERS and settings.GUNICORN_TIMEOUT aren't working here because
