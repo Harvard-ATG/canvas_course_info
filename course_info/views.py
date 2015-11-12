4001a72fa6becfe1d739fe35eacde097335eb4f6
Dave Bonner <david_bonner@harvard.edu>
2015-11-12 16:40:49 -0500

    add missing import

--- course_info/views.py
+++ course_info/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_POST
 
-from icommons import ICommonsApi
+from icommons import ICommonsApi, ICommonsApiValidationError
 
 
 _api = ICommonsApi()
