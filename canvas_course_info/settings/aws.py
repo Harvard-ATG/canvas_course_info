e77c141ad1009732467f6c6b9e22f9e972fc1c00
Colin Murtaugh <cmurtaugh@gmail.com>
2020-06-08 11:50:49 -0400

    s3 staticfiles

--- canvas_course_info/settings/aws.py
+++ canvas_course_info/settings/aws.py
@@ -1,5 +1,4 @@
 from .base import *
-from logging.config import dictConfig
 
 ALLOWED_HOSTS = ['.tlt.harvard.edu']
 
@@ -7,4 +6,9 @@ ALLOWED_HOSTS = ['.tlt.harvard.edu']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 
-dictConfig(LOGGING)
+# AWS staticfiles storage
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = SECURE_SETTINGS.get('static_files_s3_bucket')
+AWS_QUERYSTRING_AUTH = False
+AWS_LOCATION = SECURE_SETTINGS.get('static_files_s3_prefix')
+AWS_DEFAULT_ACL = None
