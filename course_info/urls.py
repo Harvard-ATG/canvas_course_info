c70b1e135ffa4efc82c2bdd3b7017c2a46417686
Jordan Chu <jordanchu95@gmail.com>
2025-10-20 09:44:24 -0400

    urls update

--- course_info/urls.py
+++ course_info/urls.py
@@ -1,19 +1,12 @@
-from django.urls import path, re_path
-from course_info import views
+from django.urls import path
+from . import views
 
 app_name = "course_info"
 
-# urlpatterns = [
-#     path('tool_config', views.tool_config, name='tool_config'),
-#     path('lti_launch', views.lti_launch, name='lti_launch'),
-#     re_path(r'^widget', views.widget, name='widget'),
-# ]
-
 urlpatterns = [
-    path(
-        "config/<uuid:tool_uuid>/", views.ToolConfigView.as_view(), name="tool_config"
-    ),
-    path("launch/", views.CourseInfoLaunchView.as_view(), name="lti_launch"),
+    path("config/<uuid:registration_uuid>/", views.config, name="config"),
+    path("launch/", views.ApplicationLaunchView.as_view(), name="launch"),
     path("not_authorized/", views.not_authorized, name="not_authorized"),
-    re_path(r"^widget", views.widget, name="widget"),
+    path("widget/", views.widget, name="widget"),
+    path("editor/", views.editor, name="editor"),
 ]
