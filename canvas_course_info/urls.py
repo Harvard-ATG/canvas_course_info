a579df7e852b09f5c8f1e945c26f04f515d46137
Jordan Chu <jordanchu95@gmail.com>
2025-10-22 15:37:54 -0400

    add admin path

--- canvas_course_info/urls.py
+++ canvas_course_info/urls.py
@@ -1,9 +1,10 @@
+from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path, re_path
-
 import watchman.views
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path(
         "course_info/",
         include(("course_info.urls", "course_info"), namespace="course_info"),
