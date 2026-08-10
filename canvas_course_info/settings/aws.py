0be094cf6daac6d447fd734d9170f4e69f8e57a5
Colin Murtaugh <cmurtaugh@gmail.com>
2026-08-10 09:16:43 -0400

    update django storages config

--- canvas_course_info/settings/aws.py
+++ canvas_course_info/settings/aws.py
@@ -7,8 +7,24 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 
 # AWS staticfiles storage
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_STORAGE_BUCKET_NAME = SECURE_SETTINGS.get("static_files_s3_bucket")
-AWS_QUERYSTRING_AUTH = False
-AWS_LOCATION = SECURE_SETTINGS.get("static_files_s3_prefix")
-AWS_DEFAULT_ACL = None
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# AWS_STORAGE_BUCKET_NAME = SECURE_SETTINGS.get("static_files_s3_bucket")
+# AWS_QUERYSTRING_AUTH = False
+# AWS_LOCATION = SECURE_SETTINGS.get("static_files_s3_prefix")
+# AWS_DEFAULT_ACL = None
+
+s3_bucket = SECURE_SETTINGS.get("static_files_s3_bucket")
+s3_prefix = SECURE_SETTINGS.get("static_files_s3_prefix")
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "bucket_name": s3_bucket,
+            "querystring_auth": False,
+            "location": s3_prefix,
+            "custom_domain": "static.tlt.harvard.edu",
+            "default_acl": None,
+        },
+    },
+}
