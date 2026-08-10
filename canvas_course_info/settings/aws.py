fe0a269d06e8b50a37ca4bcff0f001d7a8fa33de
Colin Murtaugh <cmurtaugh@gmail.com>
2026-08-10 10:28:54 -0400

    clean up aws staticfiles config

--- canvas_course_info/settings/aws.py
+++ canvas_course_info/settings/aws.py
@@ -13,20 +13,3 @@ AWS_QUERYSTRING_AUTH = False
 AWS_LOCATION = SECURE_SETTINGS.get("static_files_s3_prefix")
 AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = "static.tlt.harvard.edu"
-
-
-# s3_bucket = SECURE_SETTINGS.get("static_files_s3_bucket")
-# s3_prefix = SECURE_SETTINGS.get("static_files_s3_prefix")
-
-# STORAGES = {
-#     "staticfiles": {
-#         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-#         "OPTIONS": {
-#             "bucket_name": s3_bucket,
-#             "querystring_auth": False,
-#             "location": s3_prefix,
-#             "custom_domain": "static.tlt.harvard.edu",
-#             "default_acl": None,
-#         },
-#     },
-# }
