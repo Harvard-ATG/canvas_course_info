6b6c1dd042603ac68085e61fcc1da9d5693752f4
Colin Murtaugh <cmurtaugh@gmail.com>
2026-08-10 10:05:44 -0400

    staticfiles storage config

--- canvas_course_info/settings/aws.py
+++ canvas_course_info/settings/aws.py
@@ -6,25 +6,27 @@ ALLOWED_HOSTS = [".tlt.harvard.edu"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 
-# AWS staticfiles storage
-# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# AWS_STORAGE_BUCKET_NAME = SECURE_SETTINGS.get("static_files_s3_bucket")
-# AWS_QUERYSTRING_AUTH = False
-# AWS_LOCATION = SECURE_SETTINGS.get("static_files_s3_prefix")
-# AWS_DEFAULT_ACL = None
+# AWS staticfiles storage - config for Django < 4.2
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_STORAGE_BUCKET_NAME = SECURE_SETTINGS.get("static_files_s3_bucket")
+AWS_QUERYSTRING_AUTH = False
+AWS_LOCATION = SECURE_SETTINGS.get("static_files_s3_prefix")
+AWS_DEFAULT_ACL = None
+AWS_S3_CUSTOM_DOMAIN = "static.tlt.harvard.edu"
 
-s3_bucket = SECURE_SETTINGS.get("static_files_s3_bucket")
-s3_prefix = SECURE_SETTINGS.get("static_files_s3_prefix")
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-        "OPTIONS": {
-            "bucket_name": s3_bucket,
-            "querystring_auth": False,
-            "location": s3_prefix,
-            "custom_domain": "static.tlt.harvard.edu",
-            "default_acl": None,
-        },
-    },
-}
+# s3_bucket = SECURE_SETTINGS.get("static_files_s3_bucket")
+# s3_prefix = SECURE_SETTINGS.get("static_files_s3_prefix")
+
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+#         "OPTIONS": {
+#             "bucket_name": s3_bucket,
+#             "querystring_auth": False,
+#             "location": s3_prefix,
+#             "custom_domain": "static.tlt.harvard.edu",
+#             "default_acl": None,
+#         },
+#     },
+# }
